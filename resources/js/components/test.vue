3950ff030b41914e61bad8794ce1eb85d2f9b90a
islambouagga <44034848+islambouagga@users.noreply.github.com>
2020-07-01 06:43:18 +0100

    Initial commit

---
+++ resources/js/components/test.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                Launch demo modal
+            </button>
+
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <form @submit.prevent="login">
+                        <div class="modal-body">
+                            ...
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Save changes</button>
+                        </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                // Create a new form instance
+                form: new Form({
+                    username: '',
+                    password: '',
+                    remember: false
+                })
+            }
+        },
+
+        methods: {
+            login () {
+                // Submit the form via a POST request
+                this.form.post('api/users')
+
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
